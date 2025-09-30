17ad1fd0dd36c2ce1b2d31699480e1deddfb372b
Le Kieu Anh Vu <vu.lekieu@hcmut.edu.vn>
2025-09-30 17:50:25 +0700

    Lab 2

--- LAB2/STM32/LAB2_ex1/Core/Src/main.c
+++ LAB2/STM32/LAB2_ex1/Core/Src/main.c
@@ -91,44 +91,21 @@ void display7SEG(int num){
 }
 
 int main(void)
-{
-  /* USER CODE BEGIN 1 */
+ {
+ HAL_Init();
+ SystemClock_Config();
 
-  /* USER CODE END 1 */
+ MX_GPIO_Init();
+ MX_TIM2_Init();
 
-  /* MCU Configuration--------------------------------------------------------*/
+ /* USER CODE BEGIN 2 */
+ HAL_TIM_Base_Start_IT(&htim2);
+ /* USER CODE END 2 */
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+ 	 while (1){
 
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_TIM2_Init();
-  MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
-  HAL_TIM_Base_Start_IT(&htim2);
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
-}
+ 	 }
+ }
 
 /**
   * @brief System Clock Configuration
@@ -221,16 +198,30 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_Pin|EN0_Pin|EN1_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : LED_Pin */
-  GPIO_InitStruct.Pin = LED_Pin;
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, a_Pin|b_Pin|c_Pin|d_Pin
+                          |e_Pin|f_Pin|g_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_Pin EN0_Pin EN1_Pin */
+  GPIO_InitStruct.Pin = LED_Pin|EN0_Pin|EN1_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : a_Pin b_Pin c_Pin d_Pin
+                           e_Pin f_Pin g_Pin */
+  GPIO_InitStruct.Pin = a_Pin|b_Pin|c_Pin|d_Pin
+                          |e_Pin|f_Pin|g_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
@@ -240,13 +231,13 @@ int counter = 100;
  {
 	 if (counter == 100){
 	  display7SEG(1);
-	  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, RESET);
-	  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, SET);
+	  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_RESET);
+	  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_SET);
 	  }
 	 else if (counter == 50){
 	  display7SEG(2);
-	  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, SET);
-	  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, RESET);
+	  HAL_GPIO_WritePin(EN0_GPIO_Port, EN0_Pin, GPIO_PIN_SET);
+	  HAL_GPIO_WritePin(EN1_GPIO_Port, EN1_Pin, GPIO_PIN_RESET);
 	  }
 	  counter = (counter > 0) ? counter-1 : 100;
  }
